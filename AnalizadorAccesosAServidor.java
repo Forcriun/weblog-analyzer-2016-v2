da6af7ffe3b4cf2ddaecd6c8af3e30303cda3566
Forcriun <forcriun@gmail.com>
2018-03-06 21:29:30 +0100

    Rama 0534.b - Commit 01

--- AnalizadorAccesosAServidor.java
+++ AnalizadorAccesosAServidor.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Clase que representa objetos capaces de leer, analizar y procesar datos de registros de acceso a
@@ -12,8 +13,7 @@ import java.util.ArrayList;
 public class AnalizadorAccesosAServidor
 {
     private ArrayList<Acceso> accesos;
-    
-    
+
     /**
      * Constructor de objetos de la clase AnalizadorAccesosAServidor
      */
@@ -21,7 +21,7 @@ public class AnalizadorAccesosAServidor
     {
         accesos = new ArrayList<>();
     }
-        
+
     /**
      * Metodo que lee y analiza un archivo de log indicado por parametro. El 
      * archivo debe estar almacenado en el directorio del proyecto.
@@ -37,7 +37,7 @@ public class AnalizadorAccesosAServidor
             while (sc.hasNextLine()) {
                 // Pasa como argumento del constructor la siguiente entrada en el log             
                 Acceso accesoActual = new Acceso(sc.nextLine());
-                
+
                 accesos.add(accesoActual);
             }
             sc.close();
@@ -46,7 +46,7 @@ public class AnalizadorAccesosAServidor
             System.out.println("Ocurrio algun error al leer el archivo.");
         }
     }
-    
+
     /**
      * Metodo que devuelve la hora a la que se producen mas accesos al
      * servidor. En caso de empate devuelve la hora mas alta.
@@ -56,15 +56,15 @@ public class AnalizadorAccesosAServidor
     public int obtenerHoraMasAccesos() 
     {
         int valorADevolver = -1;
-        
+
         if (!accesos.isEmpty()) {
             int[] accesosPorHora = new int[24];
-    
+
             for (Acceso accesoActual : accesos) {
                 int horaAccesoActual = accesoActual.getHora();
                 accesosPorHora[horaAccesoActual] = accesosPorHora[horaAccesoActual] + 1;
             }
-            
+
             int numeroDeAccesosMasAlto = accesosPorHora[0];
             int horaDeAccesosMasAlto = 0;
             for (int i = 0; i < accesosPorHora.length; i++) {
@@ -73,24 +73,58 @@ public class AnalizadorAccesosAServidor
                     horaDeAccesosMasAlto = i;
                 }
             }
-            
+
             valorADevolver = horaDeAccesosMasAlto;                      
         }
-        
+
         return valorADevolver;
     }
 
-    
-    
+    /**
+     * Metodo que devuelve la pagina web mas solicitada por los clientes. 
+     * En caso de empate devuelve cualquiera de las paginas mas solicitadas.
+     *
+     * @return    Una cadena con la direccion web mas solicitada. Si no hay datos en el registro devuelve null.
+     */
     public String paginaWebMasSolicitada() 
     {
-        return "";
+        String aDevolver = null;
+
+        // Crea un HashMap para organizar las visitas a las webs
+        HashMap<String,Integer> mapaWebs = new HashMap<>();
+
+        if(!accesos.isEmpty()){
+            for(Acceso accesoActual : accesos){
+                // Si la web a la que accede el cliente ya existe en el mapa actualiza el numero de accesos
+                if(mapaWebs.containsKey(accesoActual.getUrl())){
+                    mapaWebs.replace(accesoActual.getUrl(),mapaWebs.get(accesoActual.getUrl()) + 1);
+                }
+                // En caso contrario crea una nueva entrada para esa direccion web
+                else{
+                    mapaWebs.put(accesoActual.getUrl(),1);
+                }
+            }
+
+            // Una vez organizados los datos busca la web mas solicitada            
+            int maximoVisitas = 0;
+
+            for(String url : mapaWebs.keySet()){
+                if(mapaWebs.get(url) >= maximoVisitas){
+                    maximoVisitas = mapaWebs.get(url);
+                    aDevolver = url;
+                }  
+            }
+        }
+        else{
+            System.out.println("No existen datos de acceso al servidor");
+        }
+
+        return aDevolver;
     }
-    
+
     public String clienteConMasAccesosExitosos()
     {
         return "";
     }
-
 
 }
