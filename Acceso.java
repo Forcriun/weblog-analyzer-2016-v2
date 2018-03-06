83d7781c3782a9493d9f97cbafd7ef535fc5629a
Forcriun <forcriun@gmail.com>
2018-03-06 20:07:35 +0100

    Rama 0534.a - Commit 02

--- Acceso.java
+++ Acceso.java
@@ -8,29 +8,59 @@ import java.util.Arrays;
  */
 public class Acceso
 {
+    // La direccion ip del cliente que realiza el acceso
+    private String ip;
+    
+    // Los datos de la fecha del acceso
     private int ano;
     private int mes;
     private int dia;
     private int hora;
     private int minutos;
     
+    // La direccion web a la que accede el cliente
+    private String url;
+    
+    // El codigo de respuesta HTTP del servidor
+    private int Http;
+
     /**
      * Constructor para objetos de la clase Acceso. Recibe un String con los
      * datos del acceso y los almacena en los atributos de la clase.
      * 
-     * @param  datos  La fecha del acceso en formato "yyyy mm dd hh mm" 
+     * @param  datos  Los datos del acceso en formato "91.244.73.61 [2016 01 01 10 56] instituto/normativa.html 403" 
      */
     public Acceso(String datos)
     {
         String[] datosAcceso = datos.split(" ");
-        
-        this.ano = Integer.parseInt(datosAcceso[0]);
-        this.mes = Integer.parseInt(datosAcceso[1]);
-        this.dia = Integer.parseInt(datosAcceso[2]);
-        this.hora = Integer.parseInt(datosAcceso[3]);
-        this.minutos = Integer.parseInt(datosAcceso[4]);
+
+        if(datos.matches(".*\\[.*\\].*")){
+            this.ip = datosAcceso[0];
+            this.ano = Integer.parseInt(datosAcceso[1].substring(1,4));
+            this.mes = Integer.parseInt(datosAcceso[2]);
+            this.dia = Integer.parseInt(datosAcceso[3]);
+            this.hora = Integer.parseInt(datosAcceso[4]);
+            this.minutos = Integer.parseInt(datosAcceso[5].substring(0,1));
+            this.url = datosAcceso[6];
+            this.Http = Integer.parseInt(datosAcceso[7]);
+        }
+        else{
+            this.ano = Integer.parseInt(datosAcceso[0]);
+            this.mes = Integer.parseInt(datosAcceso[1]);
+            this.dia = Integer.parseInt(datosAcceso[2]);
+            this.hora = Integer.parseInt(datosAcceso[3]);
+            this.minutos = Integer.parseInt(datosAcceso[4]);
+        }
     }
-    
+
+    /**
+     * @return    La ip del cliente que realiza el acceso
+     */
+    public String getIp() 
+    {
+        return ip;
+    }
+
     /**
      * @return    El ano del acceso
      */
@@ -38,7 +68,7 @@ public class Acceso
     {
         return ano;
     }
-    
+
     /**
      * @return    El mes del acceso
      */
@@ -46,7 +76,7 @@ public class Acceso
     {
         return mes;
     }
-    
+
     /**
      * @return    El dia del acceso
      */
@@ -54,7 +84,7 @@ public class Acceso
     {
         return dia;
     }
-    
+
     /**
      * @return    La hora del acceso
      */
@@ -62,12 +92,28 @@ public class Acceso
     {
         return hora;
     }
-    
+
     /**
      * @return    Los minutos del acceso
      */
     public int getMinutos()
     {
         return minutos;
+    }
+
+    /**
+     * @return    La url a la que se accede
+     */
+    public String getUrl() 
+    {
+        return url;
+    }
+
+    /**
+     * @return    El codigo HTTP de respuesta del servidor
+     */
+    public int getHttp()
+    {
+        return Http;
     }
 }
